4ceec8850bb237966655c8f0c9005305f9678b54
Ajay Badgujar <ajayb@pixelimage.co.in>
2018-10-30 12:13:16 +0530

    first commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  private heroesUrl = 'http://192.168.0.30/360/selfnavv2/agserve.php';  // URL to web api
+
+  constructor(private http: HttpClient,
+    private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      catchError(this.handleError('getHeroes', []))
+    );
+
+    //return of(HEROES);
+  }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add('HeroService: fetched hero id=${id}');
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+ 
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+ 
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+ 
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
+}
---
+++ src/assets/agserve.php
@@ -0,0 +1,25 @@
+<?php
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+
+ header("Access-Control-Allow-Origin: *");
+ header('Content-Type: application/json');
+ 
+    
+  //$j = json_decode('[
+      ?>
+      [{ "id": 11, "name": "Mr. Nice" },
+      { "id": 12, "name": "Narco" },
+      { "id": 13, "name": "Bombasto" },
+      { "id": 14, "name": "Celeritas" },
+      { "id": 15, "name": "Magneta" },
+      { "id": 16, "name": "RubberMan" },
+      { "id": 17, "name": "Dynama" },
+      { "id": 18, "name": "Dr IQ" },
+      { "id": 19, "name": "Magma" },
+      { "id": 20, "name": "Tornado" }]
+      <?php
+//      ]');
+ 
+ //var_dump($j);
